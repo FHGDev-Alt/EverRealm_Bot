d4fad8413ac2c6d1edd18485eebe4797c99f0853
FHGDev <fhgdev@tutanota.com>
2018-09-20 17:18:06 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const discord = require('discord.js')
+
+module.exports.run = (bot, message, args) => {
+	const em = new discord.RichEmbed()
+	.setTitle('Testbot Help')
+	.setDescription("What can I help you with?")
+	.addField('Everyone', "Help\nPing\n")
+	.addField("Moderators", "Kick\nMute\nRestart\nBan\n")
+	.addField("Developer", "Eval\n")
+	.setColor("RANDOM")
+	.setTimestamp()
+	message.channel.send({embed: em})
+}
+
+module.exports.help = {
+	name: "help"
+}
