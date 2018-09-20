703acae9c790e87130569c768e9f06fa6af7a951
FHGDev <fhgdev@tutanota.com>
2018-09-20 17:17:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,6 @@ const snekfetch = require('snekfetch')
 const bot = new discord.Client()
 const prefix = "e;"
 // const {baselogger} = require('./logger.js')
-const embed = new discord.RichEmbed()
-.setTimestamp()
-.setColor("RANDOM")
 
 bot.commands = new discord.Collection()
 
@@ -19,6 +16,14 @@ fs.readdir('./commands/', (err, files) => {
 
 bot.on('ready', () => {
   console.log("Bot ready.")
+  bot.user.setActivity(`over EverRealm Discord | e;help`)
+  bot.guilds.forEach(async(ga, id) => {
+    if (id !== "489745002604265483") return ga.leave()
+  })
+})
+
+bot.on('guildCreate', (guild, id) => {
+  if (id !== "489745002604265483") return guild.leave()
 })
 
 bot.on('message', message => {
@@ -26,10 +31,13 @@ bot.on('message', message => {
   const args = mArray.slice(1)
   const com = mArray[0].slice(prefix.length)
   const cmd = bot.commands.get(com)
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.guild) return;
+  if (message.author.bot) return;
   
   if (cmd) {
-    cmd.run(bot, message, args, prefix, embed)
+    cmd.run(bot, message, args)
   }
 })
 
-bot.login(process.env.botToken)
+bot.login(process.env.token)
